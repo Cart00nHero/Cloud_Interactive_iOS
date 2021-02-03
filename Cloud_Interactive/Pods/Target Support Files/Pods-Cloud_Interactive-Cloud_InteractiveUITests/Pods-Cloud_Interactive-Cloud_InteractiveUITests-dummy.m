e3dda6eb79d2fcb7b1c69cfa3fc78890488ddab8
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-02-03 15:21:58 +0800

    Delete

--- Cloud_Interactive/Pods/Target Support Files/Pods-Cloud_Interactive-Cloud_InteractiveUITests/Pods-Cloud_Interactive-Cloud_InteractiveUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cloud_Interactive_Cloud_InteractiveUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Cloud_Interactive_Cloud_InteractiveUITests
-@end
--- Cloud_Interactive/Pods/Target Support Files/Pods-Cloud_InteractiveTests/Pods-Cloud_InteractiveTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cloud_InteractiveTests : NSObject
-@end
-@implementation PodsDummy_Pods_Cloud_InteractiveTests
-@end
